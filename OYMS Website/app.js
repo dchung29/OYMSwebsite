c39970e3c360df630316e0c591c6597b106eea60
Hargun Sibal <hargunsibal@gmail.com>
2021-06-16 15:14:48 -0400

    SPLASH

--- OYMS Website/app.js
+++ OYMS Website/app.js
@@ -1,29 +1,29 @@
-//  let intro=document.querySelector('.intro');
-//  let logoIntro=document.querySelector('.logo-header');
-//  let logoSpan = document.querySelectorAll('.logoIntro');
+  let intro=document.querySelector('.intro');
+  let logoIntro=document.querySelector('.logo-header');
+  let logoSpan = document.querySelectorAll('.logoIntro');
 
-//  window.addEventListener('DOMContentLoaded',()=>{
-//      setTimeout(()=>{
-//        logoSpan.forEach((span,idx)=>{
-//            setTimeout(()=>{
-//                span.classList.add('active');
-//            }, (idx+1)*400) 
-//        });
+  window.addEventListener('DOMContentLoaded',()=>{
+      setTimeout(()=>{
+        logoSpan.forEach((span,idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx+1)*400) 
+        });
       
-//        setTimeout(()=>{
-//            logoSpan.forEach((span,idx)=>{
-//                setTimeout(()=>{
-//                    span.classList.remove('active');
-//                    span.classList.add('fade');
-//                }, (idx+1) * 50)
-//           })
-//        }, 2000);
+        setTimeout(()=>{
+            logoSpan.forEach((span,idx)=>{
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx+1) * 50)
+           })
+        }, 2000);
 
-//        setTimeout(()=>{
-//            intro.style.top = '-100vh';
-//        }, 2300)
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        }, 2300)
 
-//      })
+      })
 
-//  })
+  })
 
